8710dd618f8081b723d52630ae4bc91c6dda1edd
Alex Potapov <apotapov@shoprunner.com>
2021-04-04 12:51:19 -0700

    default script pre

--- set_script.js
+++ set_script.js
@@ -1,27 +1,11 @@
-import { exec } from 'child_process';
-
-/**
- * Execute simple shell command (async wrapper).
- * @param {String} cmd
- * @return {Object} { stdout: String, stderr: String }
- */
-async function sh(cmd) {
-    return new Promise(function (resolve, reject) {
-        exec(cmd, (err, stdout, stderr) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve({ stdout, stderr });
-            }
-        });
-    });
-}
-
-async function main() {
-    let { stdout } = await sh('script=$(cat tfe_output.js)');
-    for (let line of stdout.split('\n')) {
-        console.log(`ls: ${line}`);
+const { exec } = require('child_process');
+exec('script=$(cat tfe_output.js)', (err, stdout, stderr) => {
+    if (err) {
+        //some err occurred
+        console.error(err)
+    } else {
+        // the *entire* stdout and stderr (buffered)
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
     }
-}
-
-main();
+});
