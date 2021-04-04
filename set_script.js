1744ffaa62985b4ffea4f666ccc584987033c5e4
Alex Potapov <apotapov@shoprunner.com>
2021-04-04 13:36:35 -0700

    set_script

--- set_script.js
+++ set_script.js
@@ -1,5 +1,5 @@
 const { exec } = require('child_process');
-exec('echo "script=$(cat /home/runner/work/_actions/sr-apotapov/action-terraform-cloud/master/tfe_output.js)" >> $GITHUB_ENV', (err, stdout, stderr) => {
+exec('echo ‘CF_URL<<EOF’ >> $GITHUB_ENV && echo "script=$(cat /home/runner/work/_actions/sr-apotapov/action-terraform-cloud/master/tfe_output.js)" >> $GITHUB_ENV && echo ‘EOF’ >> $GITHUB_ENV', (err, stdout, stderr) => {
     if (err) {
         //some err occurred
         console.error(err)
