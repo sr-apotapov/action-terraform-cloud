e77135ecaeb9af3ec6c8e934da3b938b0115b710
Alex Potapov <apotapov@shoprunner.com>
2021-04-04 12:44:31 -0700

    default script pre

--- set_script.sh
+++ set_script.sh
@@ -1,1 +1,11 @@
-script=$(cat tfe_output.js)
+var exec = require('child_process').exec, child;
+
+child = exec('script=$(cat tfe_output.js)',
+    function (error, stdout, stderr) {
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        if (error !== null) {
+             console.log('exec error: ' + error);
+        }
+    });
+ child();
