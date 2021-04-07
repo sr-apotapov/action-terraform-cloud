35d313c54574efacccb6feaee8bb870ddc00d399
Alex Potapov <apotapov@shoprunner.com>
2021-04-07 10:36:45 -0700

    try without my code

--- src/main.ts
+++ src/main.ts
@@ -21,14 +21,14 @@ async function run (): Promise<void> {
     // const code_scan_logs = await exec('ls ./code_scan/*.tap | xargs cat')
     // const code_scan_logs_result = code_scan_logs.replace(/#|_|/g,function(match) {return replaceChars[match];}) // cleaning up the code_scan_results
     // process.env.CONTENT = code_scan_logs_result
-    const init = await exec('terraform init') //tf init
-    console.log(init)
-    const fmt = await exec('terraform fmt -check -diff') // tf fmt with diff
-    const fmt_result = fmt.replace(/#|_|/g,function(match) {return replaceChars[match];}) // cleaning up the tf fmt results
-    console.log(fmt_result)
-    const tfplan = await exec('terraform plan -no-color') // tf plan
-    const tfplan_result = tfplan.replace(/#|_|/g,function(match) {return replaceChars[match];})
-    console.log(tfplan_result) // cleaning up the tf plan results
+    // const init = await exec('terraform init') //tf init
+    // console.log(init)
+    // const fmt = await exec('terraform fmt -check -diff') // tf fmt with diff
+    // const fmt_result = fmt.replace(/#|_|/g,function(match) {return replaceChars[match];}) // cleaning up the tf fmt results
+    // console.log(fmt_result)
+    // const tfplan = await exec('terraform plan -no-color') // tf plan
+    // const tfplan_result = tfplan.replace(/#|_|/g,function(match) {return replaceChars[match];})
+    // console.log(tfplan_result) // cleaning up the tf plan results
     // };
     // const planresult = await exec("terraform plan -no-color", (err, stdout, stderr) => {
     //   if (err) {
