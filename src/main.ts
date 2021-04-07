fc5cee17d60cc9e76ae0d51a02a4c6fe218aa547
Alex Potapov <apotapov@shoprunner.com>
2021-04-07 10:15:19 -0700

    main.ts added terraform

--- src/main.ts
+++ src/main.ts
@@ -5,18 +5,36 @@ import * as core from '@actions/core';
 import * as tfcloud from './lib/tfcloud';
 
 const writeFile = util.promisify(fs.writeFile);
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
 
 async function run (): Promise<void> {
   try {
     const tfApiToken: string = core.getInput('tf_api_token');
     const tfOrg: string = core.getInput('tf_organization');
-    const planOutput = core.getInput('tf_plan_output'); // should be input
+    // const planOutput = core.getInput('tf_plan_output'); // should be input
 
     // @todo try the better way :)
-    // exec("terraform init");
-    // const result = await exec("terraform plan");
+    // async function terraform (){
+    const init = await exec('terraform init') //tf init
+    console.log(init)
+    const fmt = await exec('terraform fmt -check -diff') // tf fmt with diff
+    var replaceChars={ "%":"%25" , "%\n":"%0A" , "\r":"%0D" , "$": "\$" , "`": "%60" }; // cleanup settings
+    const fmt_result = fmt.replace(/#|_|/g,function(match) {return replaceChars[match];}) // cleaning up the tf fmt results
+    console.log(fmt_result)
+    const tfplan = await exec('terraform plan -no-color') // tf plan
+    const tfplan_result = tfplan.replace(/#|_|/g,function(match) {return replaceChars[match];}) // cleaning up the tf plan results
+    // };
+    // const planresult = await exec("terraform plan -no-color", (err, stdout, stderr) => {
+    //   if (err) {
+    //     resolve(err);
+    //   } else {
+    //     resolve({stdout, stderr});
+    //     }
+    //   });
+    // };
 
-    const plan = await tfcloud.getJsonPlan(planOutput, tfOrg, tfApiToken);
+    const plan = await tfcloud.getJsonPlan(tfplan_result, tfOrg, tfApiToken);
 
     await writeFile('tfplan.json', Buffer.from(plan));
   } catch (error) {
