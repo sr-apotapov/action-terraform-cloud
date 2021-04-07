131f6a3e0f2515c2f7a50632f6d24a1d52b8e4a2
Alex Potapov <apotapov@shoprunner.com>
2021-04-07 10:22:50 -0700

    added code_scan_result cleanup

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,14 @@ async function run (): Promise<void> {
 
     // @todo try the better way :)
     // async function terraform (){
+    var replaceChars={ "%":"%25" , "%\n":"%0A" , "\r":"%0D" , "$": "\$" , "`": "%60" }; // cleanup settings
+
+    const code_scan_logs = await exec('ls ./code_scan/*.tap | xargs cat')
+    const code_scan_logs_result = code_scan_logs.replace(/#|_|/g,function(match) {return replaceChars[match];}) // cleaning up the code_scan_results
+    process.env.CONTENT = code_scan_logs_result
     const init = await exec('terraform init') //tf init
     console.log(init)
     const fmt = await exec('terraform fmt -check -diff') // tf fmt with diff
-    var replaceChars={ "%":"%25" , "%\n":"%0A" , "\r":"%0D" , "$": "\$" , "`": "%60" }; // cleanup settings
     const fmt_result = fmt.replace(/#|_|/g,function(match) {return replaceChars[match];}) // cleaning up the tf fmt results
     console.log(fmt_result)
     const tfplan = await exec('terraform plan -no-color') // tf plan
