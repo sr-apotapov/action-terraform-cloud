541b144bffa78a694d3c3fee5f8557bbe6112852
Alex Potapov <apotapov@shoprunner.com>
2021-04-07 17:38:40 -0700

    index updated

--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,17 @@ function getPlanApiUrl(planOutput, tfOrg) {
     if (match) {
         const urlSplit = match[0].split('/');
         return `https://app.terraform.io/api/v2/runs/${urlSplit[urlSplit.length - 1]}/plan`;
+    }  
+    if (planOutput == null) {
+      throw new Error(`planOutput is null : ${planOutput}`);
     }
+    if (planOutput == undefined){
+      throw new Error(`planOutput is undefined: ${planOutput}`);
+    }
+    if (planOutput == 'void'){
+      throw new Error(`planOutput is void: ${planOutput}`);
+    }
+  
     else {
         throw new Error(`Unable to find URL match for organization INDEX: ${tfOrg}`);
     }
